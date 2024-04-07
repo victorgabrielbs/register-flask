28f425e78ff6bbc1c9c2b3c5b9fc12c809777375
Victor Gabriel <victorgabrielbritodasilva@gmail.com>
2024-04-07 17:05:12 -0300

    Now the server uses the SQLAlchemy ORM and the name and email use encryption in the right way.

--- server.py
+++ server.py
@@ -1,37 +1,38 @@
 from flask import Flask, render_template, redirect, request
-from flask_mysqldb import MySQL
-import os
 from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+import os
 import uuid
+import hashlib
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') 
-app.config['MYSQL_DB'] = 'maindatabase'
- 
-mysql = MySQL(app)
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+class PyUser(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-
         unique_id = str(uuid.uuid4())
 
-        name = bcrypt.generate_password_hash(request.form['name']).decode('utf-8')
-        email = bcrypt.generate_password_hash(request.form['email']).decode('utf-8')
+        name = hashlib.sha256(request.form['name'].encode()).hexdigest()
+        email = hashlib.sha256(request.form['email'].encode()).hexdigest()
         password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
 
-        cursor = mysql.connection.cursor()
-        cursor.execute('INSERT INTO py_user (id, name, email, password) VALUES (%s, %s, %s, %s)', (unique_id, name, email, password))
-        mysql.connection.commit()
-     
-        cursor.close()
+        new_user = PyUser(id=unique_id, name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
 
         return redirect('/registered')
     return render_template('index.html')
@@ -40,3 +41,7 @@ def login():
 def registered():
     return render_template('registered.html')
 
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
